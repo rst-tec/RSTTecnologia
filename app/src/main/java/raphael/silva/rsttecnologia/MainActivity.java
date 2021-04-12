232b704a1b5a571a188fd95e64286d3099382ddf
AppTv <apptv.contato@gmail.com>
2021-04-12 15:53:35 -0300

    Add New Logo
    Add New Logo

--- app/src/main/java/raphael/silva/rsttecnologia/MainActivity.java
+++ app/src/main/java/raphael/silva/rsttecnologia/MainActivity.java
@@ -10,6 +10,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button btApp01;
@@ -58,6 +64,16 @@ public class MainActivity extends AppCompatActivity {
         }
 
          */
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        AdView adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
 
         getSupportActionBar().hide(); //REMOVER ACTIONBAR
 
